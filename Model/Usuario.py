b754bd2cec49d771af489bb60c3dc03d3f87422d
Angelicahaas <211061832@aluno.unb.br>
2023-06-02 21:00:37 -0300

    começando projeto

---
+++ Model/Usuario.py
@@ -0,0 +1,33 @@
+
+class Usuario():
+
+    def __init__(self, nome: str, endereco: str, telefone: int, senha: str):
+        self.__nome = nome
+        self.__endereco = endereco
+        self.__teledone = telefone
+        self.__senha = senha
+
+
+    def get_nome(self) -> str:
+       return self.__nome
+    
+    def set_nome(self, nome: str):
+       self.nome = nome
+
+    def get_endereco(self) -> str:
+       return self.__endereco
+    
+    def set_endereco(self, endereco: str):
+       self.endereco = endereco
+
+    def get_telefone(self) -> int:
+       return self.__teledone
+    
+    def set_telefone(self, telefone: int):
+       self.telefone = telefone
+
+    def get_senha(self) -> int:
+       return self.__senha
+    
+    def set_senha(self, senha: int):
+       self.senha = senha
---
+++ Model/main.py
@@ -0,0 +1,7 @@
+from PessoaFisica import PessoaFisica
+from PessoaJuridica import PessoaJuridica
+
+harleny = PessoaFisica("Harleny", "Santa", 34287736, "Leny123", 2354528593, 20, 10, 10)
+
+bomel = PessoaJuridica("Bomel" "Gama", 89567834, "190903kjn", 76590980, 1000, 0000, 8909)
+
